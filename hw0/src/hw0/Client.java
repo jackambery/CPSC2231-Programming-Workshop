1b97ab19f6dc2157f32de6936144169a6a5b468b
Jack Ambery <jack.ambery@gmail.com>
2022-09-08 17:11:24 -0400

    completed hw0

---
+++ hw0/src/hw0/Client.java
@@ -0,0 +1,19 @@
+/*
+ * Name: Jack Ambery
+ * Assignment: hw0
+ * Date: 7 Sept 2022
+ * 
+ * This is the client class for hw0.
+ */
+
+package hw0;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Greeting object = new Greeting();
+		object.sayHello();
+
+	}
+
+}
---
+++ hw0/src/hw0/Greeting.java
@@ -0,0 +1,16 @@
+/*
+ * Name: Jack Ambery
+ * Assignment: hw0
+ * Date: 7 Sept 2022
+ * 
+ * This is the main class for hw0.
+ */
+
+package hw0;
+
+public class Greeting {
+	
+	public void sayHello() {
+		System.out.println("Hello from instance of object.");
+	}
+}
