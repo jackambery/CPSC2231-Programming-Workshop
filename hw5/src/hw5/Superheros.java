031cc82f606e84e85d747ced91ea5831846aa0ae
Jack Ambery <jack.ambery@gmail.com>
2022-10-13 00:29:18 -0400

    progress on hw5

---
+++ hw5/src/hw5/Superheros.java
@@ -0,0 +1,48 @@
+package hw5;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Superheros {
+	
+	private Map<String, String> heroMap;
+	private Set<String> powerSet;
+	
+	public Superheros() {
+		heroMap = new HashMap<String, String>();
+		powerSet = new HashSet<String>();
+	}
+	
+	public void putEntryInMap(String key, String value) {
+		
+	}
+	
+	public String getEntryFromMap(String key) {
+		return "";
+	}
+	
+	public void removeEntryFromMap(String key) {
+		
+	}
+	
+	public void displayAllMapEntries() {
+		
+	}
+	
+	public void addToSet(String name) {
+		
+	}
+	
+	public void removeFromSet(String name) {
+		
+	}
+	
+	public void printSet() {
+		
+	}
+	
+	
+
+}
---
+++ hw5/src/hw5/TestHarness.java
@@ -0,0 +1,11 @@
+package hw5;
+
+public class TestHarness {
+
+	public static void main(String[] args) {
+
+		Superheros s = new Superheros();
+
+	}
+
+}
