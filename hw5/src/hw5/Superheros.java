a82f9dcd30c2458b2c501bef76d0868744a894d4
Jack Ambery <jack.ambery@gmail.com>
2022-10-17 23:12:43 -0400

    hw5 done

--- hw5/src/hw5/Superheros.java
+++ hw5/src/hw5/Superheros.java
@@ -2,6 +2,7 @@ package hw5;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
@@ -16,31 +17,40 @@ public class Superheros {
 	}
 	
 	public void putEntryInMap(String key, String value) {
-		
+		heroMap.put(key, value);
 	}
 	
 	public String getEntryFromMap(String key) {
-		return "";
+		String val = heroMap.get(key);
+		return val;
 	}
 	
 	public void removeEntryFromMap(String key) {
-		
+		heroMap.remove(key);
+		System.out.println(key + " is in the map: " + heroMap.containsKey(key));
 	}
 	
 	public void displayAllMapEntries() {
-		
+		Set<String> keys = heroMap.keySet();
+	
+		for (String key : keys) {
+			System.out.println("Hero: " + key);
+			System.out.println(" Superpower: " + heroMap.get(key) + "\n");
+		}
 	}
 	
 	public void addToSet(String name) {
-		
+		powerSet.add(name);
 	}
 	
 	public void removeFromSet(String name) {
-		
+		powerSet.remove(name);
 	}
 	
 	public void printSet() {
-		
+		for (String power : powerSet) {
+			System.out.println("Power: " + power);
+		}
 	}
 	
 	
--- hw5/src/hw5/TestHarness.java
+++ hw5/src/hw5/TestHarness.java
@@ -5,7 +5,40 @@ public class TestHarness {
 	public static void main(String[] args) {
 
 		Superheros s = new Superheros();
-
+		
+		//map testing
+		s.putEntryInMap("superman", "super strength");
+		s.putEntryInMap("spiderman", "shoot webs");
+		s.putEntryInMap("hulk", "hulk smash");
+		s.putEntryInMap("captain america", "great fighter");
+		s.putEntryInMap("ironman", "insane gadgets");
+		
+		s.displayAllMapEntries();
+		
+		String hulkPower = s.getEntryFromMap("hulk");
+		System.out.println("Hulk's power: " + hulkPower + "\n");
+		
+		System.out.println("Removing Captain America...");
+		s.removeEntryFromMap("captain america");
+		System.out.println("");
+		s.displayAllMapEntries();
+		
+		//set testing
+		s.addToSet("shapeshifting");
+		s.addToSet("invisibility");
+		s.addToSet("super speed");
+		s.addToSet("flight");
+		s.addToSet("flight");
+		s.addToSet("flight");
+		s.addToSet("super speed");
+		
+		s.printSet();
+		System.out.println("");
+		System.out.println("Removing flight...");
+		s.removeFromSet("flight");
+		
+		s.printSet();
+		
 	}
 
 }
